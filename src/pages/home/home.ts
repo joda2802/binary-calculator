d1304da6020842f079e293a85d5806028eb3bb33
joda2802 <johannessteppe281@gmail.com>
2017-09-23 19:37:55 +0200

    loole

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,6 +7,26 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  rii = "";
+  get score(){
+    const bam = this.rii
+    let sum=0;
+    let x=1;
+    let check= 0;
+
+    while (x<bam.length+1)
+	{
+    check =(( parseInt(bam)% Math.pow(10,x))-(parseInt(bam) % Math.pow(10,x-1)))/ (Math.pow(10,x-1))
+
+	  if(check==1 || check ==0)sum += ((parseInt(bam) % (Math.pow(10,x)))-(parseInt(bam) % (Math.pow(10,x-1))))/ (Math.pow(10,x-1))* Math.pow(2,x-1);
+    else return "Only use one and zero!";
+
+	  x++;
+
+
+	}
+  return sum
+  }
   constructor(public navCtrl: NavController) {
 
   }
