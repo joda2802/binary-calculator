38c71d0dd86790011de30c23983b5169dfea76dc
joda2802 <johannessteppe281@gmail.com>
2017-09-24 21:14:15 +0200

    Dezizubin

--- src/pages/BintoDez/BintoDez.ts
+++ src/pages/BintoDez/BintoDez.ts
@@ -8,24 +8,23 @@ import { NavController } from 'ionic-angular';
 export class BintoDezPage {
 
   rii = "";
-  get score(){
+  get score() {
     const bam = this.rii
-    let sum=0;
-    let x=1;
-    let check= 0;
+    let sum = 0;
+    let x = 1;
+    let check = 0;
 
-    while (x<bam.length+1)
-	{
-    check =(( parseInt(bam)% Math.pow(10,x))-(parseInt(bam) % Math.pow(10,x-1)))/ (Math.pow(10,x-1))
+    while (x < bam.length + 1) {
+      check = ((parseInt(bam) % Math.pow(10, x)) - (parseInt(bam) % Math.pow(10, x - 1))) / (Math.pow(10, x - 1))
 
-	  if(check==1 || check ==0)sum += ((parseInt(bam) % (Math.pow(10,x)))-(parseInt(bam) % (Math.pow(10,x-1))))/ (Math.pow(10,x-1))* Math.pow(2,x-1);
-    else return "Only use one and zero!";
+      if (check == 1 || check == 0) sum += ((parseInt(bam) % (Math.pow(10, x))) - (parseInt(bam) % (Math.pow(10, x - 1)))) / (Math.pow(10, x - 1)) * Math.pow(2, x - 1);
+      else return "Only use one and zero!";
 
-	  x++;
+      x++;
 
 
-	}
-  return sum
+    }
+    return sum
   }
   constructor(public navCtrl: NavController) {
 
--- src/pages/DeztoBin/DeztoBin.ts
+++ src/pages/DeztoBin/DeztoBin.ts
@@ -7,9 +7,44 @@ import { NavController } from 'ionic-angular';
 })
 export class DeztoBinPage {
 
+  rii = "";
+  get score() {
+    const bam = this.rii
+    let between = 0;
+    let num = 0;
+    let sum = "";
+    let calc = 0;
+    let fin ="";
+    let y=1;
 
-  constructor(public navCtrl: NavController) {
+    calc = parseInt(bam)
+
+    while(calc>=1)
+	{
+	sum += calc % 2
+	num = calc % 2
+	  calc =(calc -1* num )/2
+  }
+  while (y< sum.length+1){
+
+      fin += ((parseInt(sum) % Math.pow(10,y))-(parseInt(sum) % Math.pow(10,y-1)))/ (Math.pow(10,y-1))
+
+   y++
+    }
+
+    return fin
+
+
+
+
+
+
+
+
 
   }
+  constructor(public navCtrl: NavController) {
+
+ }
 
 }
